3043b8a44086100920e1ea0f6e48b8e27aa523d2
Cristian Rodriguez <cristianrodrig149@gmail.com>
2023-05-29 00:28:26 +0200

    ✨feat: Add home pages and new client

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,28 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import NuevoCliente from "./pages/NuevoCliente";
+import Inicio from "./pages/Inicio";
 
-const router = createBrowserRouter([{ path: "/", element: <h1>hello</h1> }]);
+const router = createBrowserRouter([
+  { path: "/", 
+  element: <Layout />,
+  children: [
+    { path: "/",
+      element: <Inicio />
+  },
+    { path: "/clientes/nuevo", 
+      element: <NuevoCliente /> 
+
+    },
+  ]   
+},
+
+
+
+
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ src/pages/NuevoCliente.jsx
@@ -0,0 +1,9 @@
+
+
+export default function NuevoCliente() {
+  return (
+    <div>
+        <h1 className="text-3xl font-light mb-4">Nuevo Cliente</h1>
+    </div>
+  )
+}
